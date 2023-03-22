c2fb0db7d2db50b27019b12d771ab12fea5e3e39
bwty <whalelephant@users.noreply.github.com>
2023-03-21 19:47:19 -0700

    feat: upload wasm

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -5,17 +5,6 @@ source .env
 
 echo $CHAIN_ID
 
-# Using a local Juno
-if [[ "$CHAIN_ID" == "juno_local" ]]; then
-
-echo "👀 Checking if you have have a local node setup"
-command -v docker >/dev/null 2>&1 || { echo >&2 "Docker is not installed on your machine, local Juno node can't be ran. Install it from here: https://www.docker.com/get-started"; exit 1; }
-NODE_1=`docker ps -a --format="{{.Names}}" | grep $CHAIN_ID | awk '{print $1}'`
-
-if [[ "$NODE_1" == "" ]]; then 
-	echo "Node not found"
-	exit 1
-fi 
 if ! junod keys show deployer --keyring-backend=test; then
   (
     echo "$UPLOAD_USER_MNEMONICS"
@@ -23,13 +12,27 @@ if ! junod keys show deployer --keyring-backend=test; then
   ) | junod keys add deployer --recover --keyring-backend=test 
 fi
 
-REST=$(junod tx wasm store artifacts/cronkitty.wasm --gas auto --gas-prices 0.025ujunox --gas-adjustment 1.3 --from deployer --keyring-backend test --chain-id "juno-local"  -o json -y)
+#  Check we have one runnint to use a local Juno
+if [[ "$CHAIN_ID" == "juno_local" ]]; then
 
+  echo "👀 Checking if you have have a local node setup"
+  command -v docker >/dev/null 2>&1 || { echo >&2 "Docker is not installed on your machine, local Juno node can't be ran. Install it from here: https://www.docker.com/get-started"; exit 1; }
+  NODE_1=`docker ps -a --format="{{.Names}}" | grep $CHAIN_ID | awk '{print $1}'`
+  
+  if [[ "$NODE_1" == "" ]]; then 
+  	echo "Node not found"
+  	exit 1
+  fi 
+fi
+  
+REST=$(junod tx wasm store artifacts/cronkitty.wasm --gas=auto --gas-prices=0.025ujunox --gas-adjustment=1.3 --from=deployer --keyring-backend=test --chain-id=$CHAIN_ID -o=json -y -b sync --node=$RPC_URL)
+
+echo "Got tx hash"
 echo $REST
 
-sleep 6
+sleep 25
 
-RESQ=$(junod q tx --type=hash $(echo "$REST"| jq -r '.txhash') -o json)
+RESQ=$(junod q tx --node=$RPC_URL --type=hash $(echo "$REST"| jq -r '.txhash') -o json)
 
 echo $RESQ
 
@@ -42,16 +45,6 @@ echo $CODE_ID
 
 echo "Code hash:"
 echo $CODE_HASH
-
-elif [[ "$CHAIN_ID" == "juno_testnet" ]]; then
-	echo "Unsupported chain - coming soon"
-
-elif [[ "$CHAIN_ID" == "juno_mainnet" ]]; then
-	echo "Unsupported chain - coming soon"
-else 
-	echo "Unsupported chain - coming soon"
-fi
-
 
 
 
